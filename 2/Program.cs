d15236a91500d373404b2ce61053d58e34f79641
narek <narek@DESKTOP-98JS365>
2024-06-20 10:45:11 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double radius = 5.0;
+        double area = CalculateCircleArea(radius);
+        Console.WriteLine($"Площадь окружности с радиусом {radius} равна {area}");
+    }
+
+    static double CalculateCircleArea(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+}
